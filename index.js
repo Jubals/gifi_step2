6fea24721aadc4ec0d515604d62833c8560b65cb
Jubals <yuvalss92@gmail.com>
2016-02-20 19:12:06 +0200

    Added files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		height: '390',
+		width: '640',
+		videoId: 'S5aTVfVVZjU',
+		events: {
+			'onReady': onPlayerReady
+		}
+	});
+
+	document.getElementById("resume").onclick = function() {
+		player.playVideo()
+	};
+	document.getElementById("pause").onclick = function() {
+		player.pauseVideo()
+	}
+
+	function onPlayerReady(c) {
+    c.target.playVideo()
+}
+}
+
+$(document).ready(function() {
+    var c = ['<option value="none" disabled selected>Select a friend</option>'];
+    $.getJSON("http://learn.gifi.co.il/api/users/", function(d) {
+        
+        $.each(d.users, function(a, b) {
+            c.push("<option value='" + b.video + "'>" + b.fullname + "</option>")
+        });
+        $("#users").html(c.join("")).change(function() {
+            var a = $(this).val().split("v=")[1],
+                b = a.indexOf("&"); - 1 != b && (a = a.substring(0, b));
+            console.log(a);
+            player.loadVideoById(a)
+        })
+    })
+});
